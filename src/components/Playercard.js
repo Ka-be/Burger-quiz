81f72598938050f4190031eedd756fdf51d37e1f
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2023-05-17 16:50:48 +0200

    add listener to update playerName

--- src/components/Playercard.js
+++ src/components/Playercard.js
@@ -1,30 +1,51 @@
-import React, {useState} from "react";
+import { useEffect } from "react";
+import React, { useState } from "react";
 
 const Playercard = () => {
-  const [borderColor, setBorderColor] = useState(""); // État pour stocker la couleur de la bordure
+	const [borderColor, setBorderColor] = useState(""); // État pour stocker la couleur de la bordure
+  let [playerName, setPlayerName] = useState("");
 
-  const mayoClick = () => {
-    setBorderColor("mayo"); // Met à jour l'état avec la couleur "mayo"
-  };
+	const mayoClick = () => {
+		setBorderColor("mayo"); // Met à jour l'état avec la couleur "mayo"
+	};
 
-  const ketchupClick = () => {
-    setBorderColor("ketchup"); 
-  };
+	const ketchupClick = () => {
+		setBorderColor("ketchup");
+	};
 
+	useEffect(() => {
+		const playerNameInput = document.querySelector("#playername");
+
+		playerNameInput.addEventListener("blur", () => {
+			setPlayerName(playerNameInput.value);
+		});
+	});
 
 	return (
 		<div className={`playercard ${borderColor}`}>
-      <div className="playercard-name">
-        <label htmlFor="playername">
-				Donnes ton prénom (michel_boujenah_du_54 ça marche aussi).
-			  </label>
-			  <input type="text" name="playername" id="playername" />
-      </div>
-			<div className="playercard-team">
-				<input type="button" value="Team Mayo" id="mayo-btn" onClick={mayoClick} />
-				<input type="button" value="Team Ketchup" id="ketchup-btn" onClick={ketchupClick}/>
+			<div className="playercard-name">
+				<label htmlFor="playername">
+					Donnes ton prénom (michel_boujenah_du_54 ça marche aussi).
+				</label>
+				<input type="text" name="playername" id="playername" />
 			</div>
-      <button type="button" id="run-btn">C'est parti !</button>
+			<div className="playercard-team">
+				<input
+					type="button"
+					value="Team Mayo"
+					id="mayo-btn"
+					onClick={mayoClick}
+				/>
+				<input
+					type="button"
+					value="Team Ketchup"
+					id="ketchup-btn"
+					onClick={ketchupClick}
+				/>
+			</div>
+			<button type="button" id="run-btn">
+				C'est parti {playerName} !
+			</button>
 		</div>
 	);
 };
