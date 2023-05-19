e2b395f8c6b71b0a8e5b3fb4e1397d31474e6f06
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2023-05-19 14:39:48 +0200

    add use effect on answers in quizcard

--- src/components/Quizcard.js
+++ src/components/Quizcard.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Quizcard = () => {
+
+  useEffect(() =>{
+    const answers = Array.from(document.getElementsByClassName('answer'));
+    answers.forEach(element => {
+      element.addEventListener('click', () =>{
+        element.style.background = 'blue';
+        // Ajouter un useState 'checked' - si checked background bleu sinon reste en gris
+      })
+    });
+  })
+
   return (
     // Add variable for border color of card (Use Redux)
     <div className='quizcard mayo'>
       <div className="quizcard-question">
-        Quelle est la couleur du cheval blanc d'Henri IV ?
+      Une seule de ces affirmations est vraie, laquelle ?
       </div>
       <div className="quizcard-answers">
-        <div className="answer">Réponse 1</div>
-        <div className="answer">Réponse 2</div>
-        <div className="answer">Réponse 3</div>
-        <div className="answer">Réponse 4</div>
+        <div className="answer">Richard Nixon a traversé la douane avec une mallette pleine de weed pour Louis Armstrong.</div>
+        <div className="answer">John F. Kennedy a traversé la douane avec une mallette pleine de coke pour Elvis Presley.</div>
+        <div className="answer">Barack Obama a traversé la douane avec une mallette pleine de LSD pour Jay-Z.</div>
+        <div className="answer">Donald Trump a traversé la douane pour raccompagner un mexicain chez lui.</div>
       </div>
     </div>
   );
